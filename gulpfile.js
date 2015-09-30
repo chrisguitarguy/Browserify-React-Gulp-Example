eca9e55a60f67979ab347133a4791de3c18a5aef
Christopher Davis <cdavis9999@gmail.com>
2015-09-30 08:23:30 -0400

    Minify Things

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,34 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
-var argv = require('yargs').argv;
+var uglify = require('gulp-uglify');
 
-gulp.task('browserify', function() {
+gulp.task('compile-dev', function() {
     gulp.src('index.js', {read: false})
         .pipe(browserify({
             insertGlobals: true,
             transform: ['babelify'],
-            debug: !argv.production
+            debug: true
         }))
         .pipe(rename('app.js'))
         .pipe(gulp.dest('./build'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(['*.js', 'src/**/*.js'], ['browserify']);
+gulp.task('compile', function() {
+    gulp.src('index.js', {read: false})
+        .pipe(browserify({
+            insertGlobals: true,
+            transform: ['babelify'],
+            debug: false
+        }))
+        .pipe(uglify())
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('./build'));
+
 });
 
-gulp.task('default', ['browserify']);
+gulp.task('watch', function() {
+    gulp.watch(['*.js', 'src/**/*.js'], ['compile-dev']);
+});
+
+gulp.task('default', ['compile']);
